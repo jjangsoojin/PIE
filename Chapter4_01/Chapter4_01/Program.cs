f08afe9a43c935e886046ef98c61d90b668892c4
Soojin Ahn <121205497+jjangsoojin@users.noreply.github.com>
2023-12-26 21:06:26 +0900

    Chapter4_01

---
+++ Chapter4_01/Chapter4_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        Exam stack = new Exam();
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        stack.PrintStack();
+
+        stack.Pop();
+
+        stack.PrintStack();
+
+        stack.Push(4);
+        stack.Push(5);
+        stack.Push(6);
+
+        stack.PrintStack();
+    }
+}

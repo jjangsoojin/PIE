3b527859520a282c68f12bfb960066b5575347cf
Soojin Ahn <121205497+jjangsoojin@users.noreply.github.com>
2024-01-13 23:18:11 +0900

    PIE

---
+++ chpater6_01/chpater6_01/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chpater6_01
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Class1
+    {
+        public char MiddleNode(string str)
+        {
+            Dictionary<char, int> charHash = new Dictionary<char, int>();
+            
+            // 문자 횟수 체크
+            foreach (char c in str)
+            {
+                if (charHash.ContainsKey(c))
+                {
+                    charHash[c]++;
+                }
+                else
+                {
+                    charHash[c] = 1;              
+                }
+            }
+           
+            // 한번만 쓰인 문자 체크
+            foreach (char c in str)
+            {
+                if (charHash[c] == 1)
+                {
+                    return c;
+                }
+            }
+            return ' ';
+        }
+    }
+}
---
+++ chpater6_01/chpater6_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace chpater6_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1
+            Class1 cal = new Class1();
+            char c = cal.MiddleNode("teeter");
+            Console.WriteLine(c);
+        }
+    }
+}
